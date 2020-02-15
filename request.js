cec64621e4a7fe055daba117bdbef63a2b213c03
nimesh12maheshwari <45997691+nimesh12maheshwari@users.noreply.github.com>
2020-02-15 13:52:21 +0530

    changes in background.js

--- request.js
+++ request.js
@@ -1,3 +1,24 @@
+var cur_hdm=[],last_hdm=[],hour_p=[],hour_t=[],day_p=[],day_t=[],month_p=[],month_t=[],productive_categories=[];
+var d=new Date();
+var last=d.getMinutes();
+last_hdm[0]=d.getHours();last_hdm[1]=d.getDate();last_hdm[2]=d.getMonth();
+function getCurrentTime(){
+    cur_hdm[0]=d.getHours();cur_hdm[1]=d.getDate();cur_hdm[2]=d.getMonth()+1;
+    chrome.storage.sync.get(['last_time','hour_t','hour_p','day_t','day_p','month_p','month_t','poductive_categories'],function(data)
+        {
+            last_hdm=data.last_time;
+            hour_p=data.hour_p;
+            hour_t=data.hour_t;
+            day_p=data.day_p;
+            day_t=data.day_t;
+            month_p=data.month_p;
+            month_t=data.month_t;
+            productive_categories=data.poductive_categories;
+        }
+    );
+    chrome.storage.sync.set({"last_time":cur_hdm});
+    
+}
 //Receive from url get
 var htmlPage='';
 // Response from api (object)
@@ -132,20 +153,17 @@ function checkStatus()
     return -1;
     var tot=0;
     var i;
-    var productive_categories=[];
-    chrome.storage.local.get(["poductive_categories"],function(data){
-        productive_categories = data.poductive_categories;
-        console.log('inside sync');
-        console.log(productive_categories);
-    });
+    console.log(productive_categories);
     console.log(responseObject);
     //console.log('productive_cate : '+productive_categories);
 
 
     for(i=0;i<productive_categories.length;i++)
     {
-        if(productive_categories[i] in responseObject)
-        tot += responseObject[productive_categories[i]];
+        if(productive_categories[i] in responseObject){
+            console.log(productive_categories[i],responseObject[productive_categories[i]]);
+            tot += Number(responseObject[productive_categories[i]]);
+        }
     }
     
     console.log("tot: "+tot)
