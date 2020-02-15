c1c9371f3a647d83b3bf083f785d8ce0517d16a9
nimesh12maheshwari <45997691+nimesh12maheshwari@users.noreply.github.com>
2020-02-15 06:44:21 +0530

    add hashANDcurData

---
+++ hashANDcurData.js
@@ -0,0 +1,29 @@
+window.urlHash = {}         //urlHAsh
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  window.urlHash[request.url] = request.count
+})
+
+//chrome.browserAction.onClicked.addListener(function (tab) {
+//  chrome.tabs.create({url: 'tab_popup.html'})
+//});
+var cur_hdm=[],last_hdm=[],hour_p=[],hour_t=[],day_p=[],day_t=[],month_p=[],month_t=[],productive_categories=[];
+var d=new Date();
+var last=d.getMinutes();
+last_hdm[0]=d.getHours();last_hdm[1]=d.getDate();last_hdm[2]=d.getMonth();
+function getCurrentTime(){
+    cur_hdm[0]=d.getHours();cur_hdm[1]=d.getDate();cur_hdm[2]=d.getMonth()+1;
+    chrome.storage.sync.get(['last_time','hour_t','hour_p','day_t','day_p','month_p','month_t','poductive_categories'],function(data)
+        {
+            last_hdm=data.last_time;
+            hour_p=data.hour_p;
+            hour_t=data.hour_t;
+            day_p=data.day_p;
+            day_t=data.day_t;
+            month_p=data.month_p;
+            month_t=data.month_t;
+            productive_categories=data.poductive_categories;
+        }
+    );
+    chrome.storage.sync.set({"last_time":cur_hdm});
+    
+}
