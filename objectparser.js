6ab52569dfea1a0988f080d985387c7c9cd3afd3
abhi-biswas <mpsabhijeet@gmail.com>
2020-02-15 19:53:13 +0530

    object parser updated

--- objectparser.js
+++ objectparser.js
@@ -2,12 +2,24 @@ var responseObject;
 
 function extractObjectFromXMLString(string)
 {
-    //console.log("Object Extractor Ran ");
+    console.log("Object Extractor Ran ");
     var obj = {};
     var find = "className=";
     var curr = 0;
+    var k = 154;
+    var status = "";
+    while(string[k]!='"')
+    {
+        status += string[k++];
+    }
+    console.log(status);
+    if(status!='2000')
+    {
+        //return undefined;
+    }
     for(var i = 0; i < string.length; i++)
     {
+        //console.log(i,string[i]);
         if(string[i]==find[curr])
         {
             curr++;
